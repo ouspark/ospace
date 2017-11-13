c273b3ebecc6d4a109269a6b644f4361268269a4
Spark Ou <spark.ou@missionsky.com>
2017-11-13 18:24:17 +0800

    ospace initial commit

---
+++ client/src/main/scala/com/ouspark/playscalajs/ScalaJSExample.scala
@@ -0,0 +1,11 @@
+package com.ouspark.playscalajs
+
+import com.ouspark.playscalajs.shared.SharedMessages
+import org.scalajs.dom
+
+object ScalaJSExample {
+
+  def main(args: Array[String]): Unit = {
+    dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
+  }
+}
---
+++ shared/src/main/scala/com/ouspark/playscalajs/shared/SharedMessages.scala
@@ -0,0 +1,5 @@
+package com.ouspark.playscalajs.shared
+
+object SharedMessages {
+  def itWorks = "It works!"
+}
